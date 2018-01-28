ed246527ee721325590aef50e59c3e3aa4724655
radugoada <32519950+radugoada@users.noreply.github.com>
2018-01-28 23:53:09 +0200

    Redesign frame layout because I got some errors Resolved 'R' symbol issue Added open camera method and Camera hardware initialized with the 'camera' variable Added new Java class (with Constructor and Destructor) for opening the rear camera

--- app/src/main/java/com/company/radugoada/stillcam/MainActivity.java
+++ app/src/main/java/com/company/radugoada/stillcam/MainActivity.java
@@ -1,13 +1,27 @@
 package com.company.radugoada.stillcam;
 
+import android.annotation.SuppressLint;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Camera camera;
+    FrameLayout frameLayout;
+
+    //@SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frameLayout=(FrameLayout)findViewById(R.id.frameLayout);
+
+        //Open the Rear Camera
+
+        camera = Camera.open();
+
+
     }
 }
