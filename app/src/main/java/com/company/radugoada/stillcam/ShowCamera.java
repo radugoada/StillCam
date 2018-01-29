3a09faaa86cff29a6bf6295cbc4b3c3121019a38
radugoada <radu.goada@gmail.com>
2018-01-29 12:06:24 +0200

    Set the maximum resolution of the device camera Added block comments on every class with the text from MIT License

--- app/src/main/java/com/company/radugoada/stillcam/ShowCamera.java
+++ app/src/main/java/com/company/radugoada/stillcam/ShowCamera.java
@@ -7,9 +7,22 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
-/**
- * Created by radugoada on 1/28/18.
+/*
+ MIT License
+
+ Copyright (c) 2018 Radu Goadă
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
  */
 
 public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
@@ -41,6 +54,15 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
         //setting parameters for the Camera itself
         Camera.Parameters params = camera.getParameters();
 
+        //initialize camera to get the resolution sizes
+        List<Camera.Size> sizes = params.getSupportedPictureSizes();
+        Camera.Size mSize = null;
+
+        for(Camera.Size size : sizes)
+        {
+            mSize = size;
+        }
+
         //change orientation of the device camera
         if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
 
@@ -57,7 +79,7 @@ public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
             params.setRotation(90);
 
         }
-
+        params.setPictureSize(mSize.width, mSize.height); //set parameters for resolution image size Width and Height
         camera.setParameters(params);
         try{
 
